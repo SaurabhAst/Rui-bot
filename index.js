b9530ef71274e2ec3a27425dc3f003f5905d5ffc
SaurabhAst <saurabhasthana008@gmail.com>
2024-10-23 01:49:12 +0530

    added add and sub maths functions

--- index.js
+++ index.js
@@ -10,18 +10,48 @@ const client = new Client({
   ],
 });
 
-// Event: When the bot is ready
+
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-// Event: When a message is received
-client.on('messageCreate', (message) => {
-  if (message.author.bot) return; // Ignore bot messages
+const commands = {
+  add : (args) =>{
+    if (args.length < 2){
+      return 'you atleast need to give two numbers, Dummy!';
+    }
+    const num1 = parseFloat(args[0]);
+    const num2 = parseFloat(args[1]);
+    if (isNaN(num1) || isNaN(num2)){
+      return 'baka! give me numbers to add'
+    }
+    return `the sum is ${num1 + num2}`;
+  },
+  sub : (args) =>{
+    if (args.length < 2){
+      return 'you atleast need to give two numbers, Dummy!';
+    }
+    const num1 = parseFloat(args[0]);
+    const num2 = parseFloat(args[1]);
+    if (isNaN(num1) || isNaN(num2)){
+      return 'baka!, give me numbers to subtract'
+    }
+    return `the difference is ${num1 - num2}`;
+  },
+  //hello : message.channel.send('Hey there, I am Rui! also which anime are you watching currently?')
+};
 
-  // Simple command response
-  if (message.content === '!hello') {
-    message.channel.send('Hi there, I am Rui! also which anime are you watching currently?');
+client.on('messageCreate', (message) => {
+  if (!message.content.startsWith('!') || message.author.bot) return; // Ignore bot messages and ones which dont start with '!'
+
+  let args = message.content.slice(1).trim().split(/ +/); //seprated arguments
+  let command = args.shift().toLowerCase(); //shifting command to 'command'
+
+  if (command in commands){
+    const result = commands[command](args);
+    message.channel.send(result);
+  } else{
+    message.channel.send("Sorry, but I don't recognise that command");
   }
 });
 
